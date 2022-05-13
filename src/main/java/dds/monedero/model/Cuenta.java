c759818eca9836459bd9ceb1c77b8dfad208754d
Vvallejos984 <valentinvallejos.984@gmail.com>
2022-05-13 15:58:34 -0300

    Eliminados imports de excepciones en Cuenta

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -1,11 +1,5 @@
 package dds.monedero.model;
 
-import dds.monedero.exceptions.MaximaCantidadDepositosException;
-import dds.monedero.exceptions.MaximoExtraccionDiarioException;
-import dds.monedero.exceptions.MontoNegativoException;
-import dds.monedero.exceptions.SaldoMenorException;
-import dds.monedero.model.Validacion;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
